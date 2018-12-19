35ca8483946a7e50d08d11ab2a4fef71662a1f51
Chiara Passaro <chiarapassaro.wd@gmail.com>
2018-12-19 09:09:10 +0100

    modified:   index.html 	modified:   js/script.js
    add object select

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //Dati Api
 var apiKey = 'f45eed1b51907eec504d83c2a1f86cae';
 var urlApi = 'https://api.themoviedb.org/3/search/movie';
+var urlApiLanguages = 'https://api.themoviedb.org/3/configuration/languages';
 var language = 'it';
 var query = 'Aliens'; //Utente può cambiare
 
@@ -8,22 +9,36 @@ $(document).ready(function () {
 
     //dati interfaccia
     var wrapper = $('.wrapper-list');
-    var source   = $('#film__template').html();
+    var source = $('#film__template').html();
 
-    //dati da utente
-    var input = $('#search');
-    var selectL = $('#lang');
+    var wrapperSelect = $('.select__select');
+    var sourceSelect = $('#select__template').html();
+
+    //creo la select delle lingue
+    var selectLanguage = selectObject;
+    selectLanguage.setTemplate(wrapperSelect, sourceSelect);
+    selectLanguage.setData(apiKey, urlApiLanguages);
+    selectLanguage.getData();
+
 
     var submit = $('#button-film');
     submit.click(function () {
+
+        //dati da utente
+        var input = $('#search');
+        var selectL = $('#lang');
+
         query = input.val();
         language = selectL.val();
-
+        console.log(language);
         var films = filmObject;
         films.setTemplate(wrapper, source);
         films.setData(apiKey, urlApi, language, query);
         films.getData();
+
     });
+
+
 });
 
 //oggetto film
@@ -38,26 +53,8 @@ var filmObject = {
     //funzione che stampa le card
     printData(data){
             var thisObject = this;
-
+            var labels = this.labels;
             var filmsArray = data.results;
-
-
-            //array traduzione label
-            var labels = {
-                it: {
-                    'labelTitoloOriginale': 'Titolo originale',
-                    'labelAnnoUscita': 'Anno di Uscita',
-                    'labelLingua': 'Lingua',
-                    'labelVoto': 'Voto'
-                },
-                en: {
-                    'labelTitoloOriginale': 'Original Title',
-                    'labelAnnoUscita': 'Release Date',
-                    'labelLingua': 'Language',
-                    'labelVoto': 'Vote average'
-                }
-            };
-
 
             var context = {
                 querySearch : query,
@@ -84,10 +81,10 @@ var filmObject = {
                 }
 
                 context['films'][i] = {
-                    labelTitoloOriginale: labels[thisObject.language].labelTitoloOriginale,
-                    labelAnnoUscita:  labels[thisObject.language].labelAnnoUscita,
-                    labelLingua: labels[thisObject.language].labelLingua,
-                    labelVoto:  labels[thisObject.language].labelVoto,
+                    labelTitoloOriginale: labels[thisObject.languageLabel].labelTitoloOriginale,
+                    labelAnnoUscita:  labels[thisObject.languageLabel].labelAnnoUscita,
+                    labelLingua: labels[thisObject.languageLabel].labelLingua,
+                    labelVoto:  labels[thisObject.languageLabel].labelVoto,
                     filmTitle: thisFilm.title,
                     originalTitle: thisFilm.original_title,
                     filmYear: thisFilm.release_date,
@@ -105,6 +102,30 @@ var filmObject = {
         this.apiKey = apiKey;
         this.urlApi = urlApi;
         this.language = language;
+        //array traduzione label
+        this.labels = {
+            it: {
+                'labelTitoloOriginale': 'Titolo originale',
+                'labelAnnoUscita': 'Anno di Uscita',
+                'labelLingua': 'Lingua',
+                'labelVoto': 'Voto'
+            },
+            en: {
+                'labelTitoloOriginale': 'Original Title',
+                'labelAnnoUscita': 'Release Date',
+                'labelLingua': 'Language',
+                'labelVoto': 'Vote average'
+            }
+        };
+        console.log(this.language);
+        if(this.labels.hasOwnProperty(this.language)){
+          console.log('true');
+          this.languageLabel = this.language;
+        } else {
+          console.log('false');
+          this.languageLabel = 'en';
+        }
+
         this.query = query;
     },
     //funzione che chiama api
@@ -130,3 +151,61 @@ var filmObject = {
 };
 
 
+//oggetto select
+var selectObject = {
+    //funzione per passare html
+    setTemplate(wrapper, sourceTemplate){
+        var thisObject = this;
+        thisObject.wrapper = wrapper;
+        thisObject.source = sourceTemplate;
+        thisObject.template = Handlebars.compile(thisObject.source);
+    },
+    //funzione che stampa le card
+    printData(data){
+            var thisObject = this;
+
+            var array = data;
+            var context = {
+                languages: array
+            };
+
+            //console.log(context);
+            var html = thisObject.template(context);
+            thisObject.wrapper.html(html);
+
+    },
+    //funzione a cui passare i dati
+    setData(apiKey, urlApi){
+        this.apiKey = apiKey;
+        this.urlApi = urlApi;
+    },
+    //funzione che chiama api
+    getData() {
+        var thisObject = this;
+
+        $.ajax({
+            url: this.urlApi,
+            data: {
+              api_key: thisObject.apiKey
+            },
+            success: function (data) {
+                var result = data;
+                var dataLength = data.length;
+                var arrayLanguages = [];
+                var i = 0;
+
+                while(i < dataLength){
+                  arrayLanguages[i] = {
+                    name: data[i].english_name,
+                    codeLang: data[i].iso_639_1
+                  };
+                  i++;
+                }
+                return thisObject.printData(arrayLanguages);
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+    }
+};
