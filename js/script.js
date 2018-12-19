fcb3fc369fa23c486ea5533c7d0b53bf2ebe35dc
Chiara Passaro <chiarapassaro.wd@gmail.com>
2018-12-19 15:57:11 +0100

    new file:   img/en.png 	new file:   img/es.png 	new file:   img/fi.png 	new file:   img/it.png 	modified:   js/script.js
    modify selectObject
    img.onload it does not work as required

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var urlApi = 'https://api.themoviedb.org/3/search/movie';
 var urlApiLanguages = 'https://api.themoviedb.org/3/configuration/languages';
 var language = 'it';
 var query = 'Aliens'; //Utente può cambiare
-var bandierePermesse = ['it', 'gb', 'es', 'be'];
+var bandierePermesse = ['it', 'en', 'es', 'be'];
 
 $(document).ready(function () {
 
@@ -132,10 +132,8 @@ var filmObject = {
 
 
         if(this.labels.hasOwnProperty(this.language)){
-          //console.log('true');
           this.languageLabel = this.language;
         } else {
-          //console.log('false');
           this.languageLabel = 'en';
         }
 
@@ -164,102 +162,7 @@ var filmObject = {
 };
 
 
-//oggetto select
-var selectObject = {
-    //funzione per passare html
-    setTemplate(wrapper, sourceTemplate){
-        var thisObject = this;
-        thisObject.wrapper = wrapper;
-        thisObject.source = sourceTemplate;
-        thisObject.template = Handlebars.compile(thisObject.source);
-        thisObject.array = [];
-    },
-    //funzione che stampa le card
-    printData(data){
-            var thisObject = this;
-            var thisArray = data;
 
-            var context = {
-                languages: thisObject.array
-            };
-
-            var html = thisObject.template(context);
-            thisObject.wrapper.html(html);
-    },
-    //funzione a cui passare i dati
-    setData(apiKey, urlApi){
-        this.apiKey = apiKey;
-        this.urlApi = urlApi;
-    },
-    //funzione che chiama api
-    getData() {
-        var thisObject = this;
-
-        $.ajax({
-            url: this.urlApi,
-            data: {
-              api_key: thisObject.apiKey
-            },
-            success: function (data) {
-                var result = data;
-                var dataLength = data.length;
-                var arrayLanguages = [];
-
-                //contatore per while
-                var i = 0;
-                while(i < dataLength){
-
-                    //creo oggetto immagine per contyrollare se carica l'immagine
-                    var image = new Image();
-                    image.src = 'https://www.countryflags.io/' + result[i].iso_639_1 + '/flat/64.png';
-
-                    //contattore per oggetto image
-                    var j = 0;
-                    image.onload = function() {
-                      // immagine  caricata
-                      var thisUrlFlag = 'https://www.countryflags.io/' + result[j].iso_639_1 + '/flat/64.png';
-
-                      var thisLanguage = {
-                          name: result[j].english_name,
-                          codeLang: result[j].iso_639_1,
-                          urlFlag: thisUrlFlag
-                      };
-
-                      getLanguage(thisLanguage, j, dataLength);
-                      j++;
-                    };
-
-                    image.onerror = function() {
-                      // immagine non caricata
-                      var thisLanguage = {
-                          name: result[j].english_name,
-                          codeLang: result[j].iso_639_1,
-                      };
-
-                      getLanguage(thisLanguage, j, dataLength);
-                      j++;
-                    };
-
-                  i++;
-                }
-
-                function getLanguage(language, j, dataLength) {
-                  //se è l'ultima lingua stampo
-                  if(thisObject.array.length == dataLength - 1 ){
-                    return thisObject.printData(arrayLanguages);
-                  } else{
-                    thisObject.array.push(language);
-                  }
-                }
-
-            },
-            error: function (err) {
-                console.log(err);
-            }
-        });
-    }
-
-};
 
 // Funzione Bandiere - le sostituisco dopo aver creato le schede
 function getFlags(bandierePermesse){
@@ -282,7 +185,8 @@ function getFlags(bandierePermesse){
     var j = 0;
     image.onload = function() {
       // immagine  caricata
-      var thisUrlFlag = 'https://www.countryflags.io/' + lingue[j] + '/flat/64.png';
+      //var thisUrlFlag = 'https://www.countryflags.io/' + lingue[j] + '/flat/64.png';
+      var thisUrlFlag = 'img/' + lingue[j] + '.png';
 
       getThisFlag(thisUrlFlag, lingue[j], j, thisLenght);
       j++;
@@ -321,3 +225,180 @@ function changeFlag(arrayFlag){
     languageWrapper.html(flag);
   }
 }
+
+
+//modificato oggetto select
+var selectObject = {
+    //funzione per passare html
+    setTemplate(wrapper, sourceTemplate){
+        var thisObject = this;
+        thisObject.wrapper = wrapper;
+        thisObject.source = sourceTemplate;
+        thisObject.template = Handlebars.compile(thisObject.source);
+    },
+    //funzione che stampa le card
+    printData(data){
+            var thisObject = this;
+            var thisArray = data;
+
+            var context = {
+                languages: thisArray
+            };
+            console.log(context);
+
+            var html = thisObject.template(context);
+            console.log(html);
+            thisObject.wrapper.html(html);
+    },
+    //funzione a cui passare i dati
+    setData(apiKey, urlApi){
+        this.apiKey = apiKey;
+        this.urlApi = urlApi;
+    },
+    //funzione che chiama api
+    getData() {
+        var thisObject = this;
+
+        $.ajax({
+            url: this.urlApi,
+            data: {
+              api_key: thisObject.apiKey
+            },
+            success: function (data) {
+                var result = data;
+                var dataLength = data.length;
+
+                var arrayLanguages = [];
+
+                //contatore per while
+                for (var i = 0; i <= dataLength; i++) {
+                  if(i == dataLength){
+                    console.log(arrayLanguages);
+                    thisObject.printData(arrayLanguages);
+                  } else{
+                    if (bandierePermesse.includes(result[i].iso_639_1)){
+                      console.log(result[i].iso_639_1);
+                      var thisUrlFlag = 'img/' + result[i].iso_639_1 + '.png';
+                      arrayLanguages[i] = {
+                        name: result[i].english_name,
+                        codeLang: result[i].iso_639_1,
+                        urlFlag: thisUrlFlag
+                      }
+                    } else {
+                      arrayLanguages[i] = {
+                        name: result[i].english_name,
+                        codeLang: result[i].iso_639_1,
+                        urlFlag: false
+                      }
+                    }
+                  }
+                }
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+    }
+
+};
+
+
+// oggetto select la chiamata onload non funziona sempre correttamente
+// var selectObject = {
+//     //funzione per passare html
+//     setTemplate(wrapper, sourceTemplate){
+//         var thisObject = this;
+//         thisObject.wrapper = wrapper;
+//         thisObject.source = sourceTemplate;
+//         thisObject.template = Handlebars.compile(thisObject.source);
+//         thisObject.array = [];
+//     },
+//     //funzione che stampa le card
+//     printData(data){
+//             var thisObject = this;
+//             var thisArray = data;
+//
+//             var context = {
+//                 languages: thisObject.array
+//             };
+//
+//             var html = thisObject.template(context);
+//             thisObject.wrapper.html(html);
+//     },
+//     //funzione a cui passare i dati
+//     setData(apiKey, urlApi){
+//         this.apiKey = apiKey;
+//         this.urlApi = urlApi;
+//     },
+//     //funzione che chiama api
+//     getData() {
+//         var thisObject = this;
+//
+//         $.ajax({
+//             url: this.urlApi,
+//             data: {
+//               api_key: thisObject.apiKey
+//             },
+//             success: function (data) {
+//                 var result = data;
+//                 var dataLength = data.length;
+//                 var arrayLanguages = [];
+//
+//                 //contatore per while
+//                 var i = 0;
+//                 while(i < dataLength){
+//
+//                     //creo oggetto immagine per contyrollare se carica l'immagine
+//                     var image = new Image();
+//                     image.src = 'https://www.countryflags.io/' + result[i].iso_639_1 + '/flat/64.png';
+//                     //image.src = 'img/' + result[i].iso_639_1 + '.png';
+//
+//                     //contattore per oggetto image
+//                     var j = 0;
+//                     image.onload = function() {
+//                       // immagine  caricata
+//                       var thisUrlFlag = 'https://www.countryflags.io/' + result[j].iso_639_1 + '/flat/64.png';
+//                       //var thisUrlFlag = 'img/' + result[j].iso_639_1 + '.png';
+//                       var thisLanguage = {
+//                           name: result[j].english_name,
+//                           codeLang: result[j].iso_639_1,
+//                           urlFlag: thisUrlFlag
+//                       };
+//
+//                       getLanguage(thisLanguage, j, dataLength);
+//                       j++;
+//                     };
+//
+//                     image.onerror = function() {
+//                       console.log('not load');
+//
+//                       // immagine non caricata
+//                       var thisLanguage = {
+//                           name: result[j].english_name,
+//                           codeLang: result[j].iso_639_1,
+//                       };
+//
+//                       getLanguage(thisLanguage, j, dataLength);
+//                       j++;
+//                     };
+//
+//                   i++;
+//                 }
+//
+//                 function getLanguage(language, j, dataLength) {
+//                   //se è l'ultima lingua stampo
+//                   if(thisObject.array.length == dataLength - 1 ){
+//                     return thisObject.printData(arrayLanguages);
+//                   } else{
+//                     thisObject.array.push(language);
+//                   }
+//                 }
+//
+//             },
+//             error: function (err) {
+//                 console.log(err);
+//             }
+//         });
+//     }
+//
+// };
