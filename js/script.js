8201f225333add2e70a958ba8b0f0d1efa48cd7f
Chiara Passaro <chiarapassaro.wd@gmail.com>
2019-01-06 18:36:54 +0100

    Modified printData

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 //Dati Api
 var apiKey = 'f45eed1b51907eec504d83c2a1f86cae',
-  urlApi = ['https://api.themoviedb.org/3/search/movie', 'https://api.themoviedb.org/3/search/tv'],
-  urlApiLanguages = 'https://api.themoviedb.org/3/configuration/languages',
-  urlImg = 'https://image.tmdb.org/t/p/w342',
-  languageLabel = 'it', //lingua base interfaccia
-  languagesAllowed = ['it', 'en', 'es']; // per le bandiere
+    urlApi = ['https://api.themoviedb.org/3/search/movie', 'https://api.themoviedb.org/3/search/tv'],
+    urlApiLanguages = 'https://api.themoviedb.org/3/configuration/languages',
+    urlImg = 'https://image.tmdb.org/t/p/w342',
+    languageLabel = 'it', //lingua base interfaccia
+    languagesAllowed = ['it', 'en', 'es']; // per le bandiere
 
 $(document).ready(function() {
 
   //dati interfaccia
   var wrapper = $('.films__wrapper'),
-    wrapperError = $('.alert'),
-    sourceTemplate = $('#film__template').html(),
-    input = $('#search'),
-    select = $('#select-language');
+      wrapperError = $('.alert'),
+      sourceTemplate = $('#film__template').html(),
+      input = $('#search'),
+      select = $('#select-language');
 
   //dati interfaccia details
   var wrapperDetails = $('.film__details'),
@@ -47,8 +47,8 @@ $(document).ready(function() {
 
   $(document).on('click', '.film__card', function() {
     var id = $(this).attr('data-idCard'),
-      type = $(this).attr('data-type'),
-      position = $(this).position();
+        type = $(this).attr('data-type'),
+        position = $(this).position();
 
     //animazione
     wrapperDetails.addClass('active');
@@ -70,21 +70,21 @@ $(document).ready(function() {
 
 });
 
-//Funzione che prende dati da Api
+//Funzione ricorsiva che prende dati da Api
 function getData(apiKey, urlApi, query, urlImg, languageLabel, wrapper, wrapperError, sourceTemplate, isFirstQuery, count, prevNumberResult, dataPrev) {
 
   var apiKey = apiKey,
-    urlApi = urlApi,
-    query = query,
-    urlImg = urlImg,
-    languageLabel = languageLabel,
-    wrapper = wrapper,
-    wrapperError = wrapperError,
-    sourceTemplate = sourceTemplate,
-    isFirstQuery = isFirstQuery || false,
-    count = count || 0,
-    prevResult = [],
-    data = dataPrev || false;
+      urlApi = urlApi,
+      query = query,
+      urlImg = urlImg,
+      languageLabel = languageLabel,
+      wrapper = wrapper,
+      wrapperError = wrapperError,
+      sourceTemplate = sourceTemplate,
+      isFirstQuery = isFirstQuery || false,
+      count = count || 0,
+      prevResult = [],
+      data = dataPrev || false;
 
   //se risultati precedenti passo array
   if (prevNumberResult) {
@@ -96,6 +96,7 @@ function getData(apiKey, urlApi, query, urlImg, languageLabel, wrapper, wrapperE
 
   //se la funzione e' stata gia' chiamata e il risultato precedente e' 0
   if (prevResult[count] == 0 && count > 1) {
+
     // se tutti gli elementi dell'array sono uguali a 0 ritorno true
     var noResults = prevResult.reduce(function(accumulator, currentValue, currentIndex, array) {
       if (array[currentIndex - 1] === currentValue && currentValue === 0) {
@@ -110,113 +111,96 @@ function getData(apiKey, urlApi, query, urlImg, languageLabel, wrapper, wrapperE
       noResult(wrapperError);
     } else {
       //altrimenti stampo
-      printData(data, query, urlImg, languageLabel, wrapper, sourceTemplate, isFirstQuery, prevResult);
+      printData(data, query, urlImg, languageLabel, wrapper, wrapperError, sourceTemplate, isFirstQuery, prevResult);
     }
 
   }
 
   // se i risultati precedenti sono diversi da 0 stampo vale anche per la prima chiamata
   else if (prevResult[count] != 0) {
-    printData(data, query, urlImg, languageLabel, wrapper, sourceTemplate, isFirstQuery, prevResult);
+    printData(data, query, urlImg, languageLabel, wrapper, wrapperError, sourceTemplate, isFirstQuery, prevResult);
   }
 
   //se count e' inferiore alla lunghezza dell'array chiamata ajax - simulo ciclo
   if (count < urlApi.length) {
     var thisUrl = urlApi[count];
-    queryData = {
-      api_key: apiKey,
-      language: languageLabel,
-      query: query
-    };
+        queryData = {
+          api_key: apiKey,
+          language: languageLabel,
+          query: query
+        };
 
     // se prima query
     // chiamata ajax 1
     if (count == 0) {
-      $.ajax({
-        url: thisUrl,
-        method: 'GET',
-        data: queryData,
-        success: function(data) {
-          var numberResult = data.total_results;
-          prevResult.push(numberResult);
-
-          //aumento counter
-          count++;
-          //richiamo la funzione stessa
-          return getData(apiKey, urlApi, query, urlImg, languageLabel, wrapper, wrapperError, sourceTemplate, true, count, prevResult, data);
-        },
-        error: function(err) {
-          wrapper.html('Non ci sono risultati');
-        }
-      });
+      isFirstQuery = true;
+    } else {
+      isFirstQuery = false;
     }
-    // se altra query
-    // chiamata ajax altra
-    else if (count > 0) {
 
-      $.ajax({
-        url: thisUrl,
-        method: 'GET',
-        data: queryData,
-        success: function(data) {
-          var numberResult = data.total_results;
-          prevResult.push(numberResult);
+    $.ajax({
+      url: thisUrl,
+      method: 'GET',
+      data: queryData,
+      success: function(data) {
+        //conservo numero risultati in array
+        var numberResult = data.total_results;
+        prevResult.push(numberResult);
 
-          //aumento counter
-          count++;
-          //richiamo la funzione stessa
-          return getData(apiKey, urlApi, query, urlImg, languageLabel, wrapper, wrapperError, sourceTemplate, false, count, prevResult, data);
-        },
-        error: function(err) {
-          wrapper.html('Non ci sono risultati');
-        }
-      });
-
-    }
+        //aumento counter
+        count++;
+        //richiamo la funzione stessa
+        return getData(apiKey, urlApi, query, urlImg, languageLabel, wrapper, wrapperError, sourceTemplate, isFirstQuery, count, prevResult, data);
+      },
+      error: function(err) {
+        noResult(wrapperError);
+      }
+    });
   }
 }
 
 //Funzione che stampa dati
-function printData(arrayApi, query, urlImg, languageLabel, wrapper, sourceTemplate, isFirstQuery, prevResult) {
+function printData(arrayApi, query, urlImg, languageLabel, wrapper, wrapperError, sourceTemplate, isFirstQuery, prevResult) {
   var arrayApi = arrayApi,
-    query = query,
-    urlImg = urlImg,
-    languageLabel = languageLabel,
-    wrapper = wrapper,
-    sourceTemplate = sourceTemplate,
-    template = Handlebars.compile(sourceTemplate),
-    isFirstQuery = isFirstQuery || false,
-    prevResult = prevResult,
-    //label per implementare seconda lingua intefaccia
-    labels = {
-      it: {
-        'labelTitoloOriginale': 'Titolo originale',
-        'labelAnnoUscita': 'Anno di Uscita',
-        'labelLingua': 'Lingua Originale',
-        'labelVoto': 'Voto',
-        'labelType': 'Tipo'
+      query = query,
+      urlImg = urlImg,
+      languageLabel = languageLabel,
+      wrapper = wrapper,
+      wrapperError = wrapperError,
+      sourceTemplate = sourceTemplate,
+      template = Handlebars.compile(sourceTemplate),
+      isFirstQuery = isFirstQuery || false,
+      prevResult = prevResult,
+      //label per implementare seconda lingua intefaccia
+      labels = {
+        it: {
+          'labelTitoloOriginale': 'Titolo originale',
+          'labelAnnoUscita': 'Anno di Uscita',
+          'labelLingua': 'Lingua Originale',
+          'labelVoto': 'Voto',
+          'labelType': 'Tipo'
+        },
+        en: {
+          'labelTitoloOriginale': 'Original Title',
+          'labelAnnoUscita': 'Release Date',
+          'labelLingua': 'Original Language',
+          'labelVoto': 'Vote average',
+          'labelType': 'Type'
+        }
       },
-      en: {
-        'labelTitoloOriginale': 'Original Title',
-        'labelAnnoUscita': 'Release Date',
-        'labelLingua': 'Original Language',
-        'labelVoto': 'Vote average',
-        'labelType': 'Type'
-      }
-    },
-    filmsArray = arrayApi.results,
-    context = {
-      querySearch: query,
-      films: []
-    },
-    i = 0;
+      filmsArray = arrayApi.results,
+      context = {
+        querySearch: query,
+        films: []
+      },
+      i = 0;
 
   while (i < filmsArray.length) {
     var thisFilm = filmsArray[i],
-      // trasformo il voto da numero decimale a intero e da 1 a 10 a 1 a 5
-      filmVote = Math.ceil(thisFilm.vote_average / 2),
-      maxStars = 5,
-      filmStar = [];
+        // trasformo il voto da numero decimale a intero e da 1 a 10 a 1 a 5
+        filmVote = Math.ceil(thisFilm.vote_average / 2),
+        maxStars = 5,
+        filmStar = [];
 
     //creo un array con le stelle
     for (var j = 0; j < maxStars; j++) {
@@ -231,17 +215,19 @@ function printData(arrayApi, query, urlImg, languageLabel, wrapper, sourceTempla
       }
     }
 
+    //Se film original_title
     if (thisFilm.original_title) {
       var originalTitle = thisFilm.original_title,
-        filmTitle = thisFilm.title,
-        type = 'film',
-        typeUrl = 'movie';
+          filmTitle = thisFilm.title,
+          type = 'film',
+          typeUrl = 'movie';
     }
+    //se serie tv original_name
     if (thisFilm.original_name) {
       var originalTitle = thisFilm.original_name,
-        filmTitle = thisFilm.name,
-        type = 'Tv Series',
-        typeUrl = 'tv';
+          filmTitle = thisFilm.name,
+          type = 'Tv Series',
+          typeUrl = 'tv';
     }
     if (thisFilm.poster_path) {
       var poster = urlImg + thisFilm.poster_path;
@@ -266,13 +252,13 @@ function printData(arrayApi, query, urlImg, languageLabel, wrapper, sourceTempla
     i++;
   }
 
-  var html = template(context),
-    wrapperError = $('.alert');
+  var html = template(context);
 
   //se prima query
   if (isFirstQuery) {
     //svuoto alert se ci sono errori
     deleteContainer(wrapperError, 'active');
+    //sostituisco html
     wrapper.html(html);
   } else {
     //appendo risultati
@@ -294,10 +280,10 @@ function getFlags(language) {
 
 function getDetails(apiKey, language, type, id, wrapper, sourceTemplate) {
   var apiKey = apiKey,
-    language = language,
-    type = type,
-    url = 'https://api.themoviedb.org/3/' + type + '/' + id,
-    wrapper = wrapper;
+      language = language,
+      type = type,
+      url = 'https://api.themoviedb.org/3/' + type + '/' + id,
+      wrapper = wrapper;
 
   $.ajax({
     url: url,
@@ -353,11 +339,11 @@ function getDetails(apiKey, language, type, id, wrapper, sourceTemplate) {
 
 function printDetails(array, wrapper, sourceTemplate) {
   var array = array,
-    wrapper = wrapper,
-    sourceTemplate = sourceTemplate,
-    template = Handlebars.compile(sourceTemplate),
-    context = array,
-    html = template(context);
+      wrapper = wrapper,
+      sourceTemplate = sourceTemplate,
+      template = Handlebars.compile(sourceTemplate),
+      context = array,
+      html = template(context);
 
   if (Object.keys(context).length > 0) {
     //aggiungo contenuto
